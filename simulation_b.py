f882f52301d5dffd42404fc3b910d79fb789fd69
Charlotte <cmjwfeltwell@gmail.com>
2026-05-05 17:34:18 -0700

    Update simulation_b.py

--- simulation_b.py
+++ simulation_b.py
@@ -5,12 +5,12 @@ from routing.node import Node
 
 def create_order():
     items = [
-        Item(1, "gallon of milk", "large", heavy=True),
+        Item(1, "pint of milk", "medium", frozen=True),
         Item(2, "gallon of water", "large", heavy=True),
         Item(3, "frozen pizza", "medium", frozen=True, fragile=True),
         Item(4, "bag of apples", "medium", heavy=True),
-        Item(5, "loaf of bread", "medium", fragile=True),
-        Item(6, "bag of ice", "large", frozen=True)
+        Item(5, "loaf of bread", "small", fragile=True),
+        Item(6, "bag of ice", "large", frozen=True, heavy=True)
     ]
     order = Order(1, "A", items)
     return order
@@ -22,8 +22,13 @@ bagger = Foodie_Bagger(fw)
 
 print(f"Order: ")
 for i in order.getItems():
-    print(f"{i.getName()}, size={i.getSize()}, frozen={i.isFrozen()}, fragile={i.isFragile()}, heavy={i.isHeavy()}")
-
+    print(
+    f"{i.getName():<25}  "
+    f"size: {str(i.getSize()):<6}  "
+    f"frozen: {str(i.isFrozen()):<6}  "
+    f"fragile: {str(i.isFragile()):<6}  "
+    f"heavy: {str(i.isHeavy()):<6}"
+)
 
 print(f"Bagging simulation:")
 bagger.bagOrder(order)
